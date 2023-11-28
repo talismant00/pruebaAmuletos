5bbe780593ccae8fa4e9cab5b322429229d37aa8
talismant00 <pau.terraza@gmail.com>
2023-11-28 20:50:50 +0100

    Momia Animaciones

--- pruebaAmuletos/PruebaAmuletos/Assets/Scripts/Enemies/EnemigoMele.cs
+++ pruebaAmuletos/PruebaAmuletos/Assets/Scripts/Enemies/EnemigoMele.cs
@@ -9,16 +9,23 @@ public class EnemigoMele : MonoBehaviour
     public float speed;
     public float detectionRange;
     public float waitTime;
+    public float attackRange;
+    public int attackDamage;
+    public float attackDelay;
+    private float lastAttackTime;
     private int destPoint = 0;
     private Transform player;
-    private enum State { Patrolling, Chasing, Waiting }
+    private enum State { Patrolling, Chasing, Waiting, Attacking }
     private State state;
 
     private Animator animator;
+    private Rigidbody2D rb;
+
     void Start()
     {
         animator = GetComponent<Animator>();
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = GetComponent<Rigidbody2D>();
         state = State.Patrolling;
     }
 
@@ -28,7 +35,7 @@ public class EnemigoMele : MonoBehaviour
         {
             case State.Patrolling:
                 Patrol();
-                
+
                 if (player != null && Vector2.Distance(transform.position, player.position) < detectionRange)
                 {
                     state = State.Chasing;
@@ -36,14 +43,24 @@ public class EnemigoMele : MonoBehaviour
                 break;
             case State.Chasing:
                 Chase();
-                if (player == null || Vector2.Distance(transform.position, player.position) >= detectionRange)
+                if (Vector2.Distance(transform.position, player.position) >= detectionRange)
                 {
-
                     state = State.Patrolling;
+                }
+                else if (Vector2.Distance(transform.position, player.position) <= attackRange)
+                {
+                    state = State.Attacking;
                 }
                 break;
             case State.Waiting:
                 if (player != null && Vector2.Distance(transform.position, player.position) < detectionRange)
+                {
+                    state = State.Chasing;
+                }
+                break;
+            case State.Attacking:
+                Attack();
+                if (Vector2.Distance(transform.position, player.position) >= attackRange)
                 {
                     state = State.Chasing;
                 }
@@ -59,18 +76,16 @@ public class EnemigoMele : MonoBehaviour
         Vector2 target = points[destPoint].position;
         Vector2 newPos = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
         GetComponent<Rigidbody2D>().MovePosition(newPos);
-        // Actualiza las variables del animator
         animator.SetFloat("Horizontal", newPos.x - transform.position.x);
         animator.SetFloat("Vertical", newPos.y - transform.position.y);
         animator.SetBool("Moving", true);
-        // Reduce el valor aquí para que el enemigo se acerque más al punto
         if (Vector2.Distance(transform.position, target) < 0.1f)
         {
             state = State.Waiting;
+            animator.SetBool("Moving", false);
             StartCoroutine(WaitAtPoint());
         }
     }
-
 
     IEnumerator WaitAtPoint()
     {
@@ -83,10 +98,58 @@ public class EnemigoMele : MonoBehaviour
     {
         Vector2 newPos = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
         GetComponent<Rigidbody2D>().MovePosition(newPos);
-        // Actualiza las variables del animator
         animator.SetFloat("Horizontal", newPos.x - transform.position.x);
         animator.SetFloat("Vertical", newPos.y - transform.position.y);
         animator.SetBool("Moving", true);
+    }
 
+    void Attack()
+    {
+        if (Time.time > lastAttackTime + attackDelay)
+        {
+            
+            animator.SetBool("Attack", true);
+            animator.SetBool("Moving", false);
+
+
+            Debug.Log("Atacando al player");
+            
+            // Aquí es donde dañarías al jugador
+            // player.GetComponent<Player>().TakeDamage(attackDamage);
+            lastAttackTime = Time.time;
+        }
+        else
+        {
+            animator.SetBool("Attack", false);
+        }
+
+
+    }
+
+    public IEnumerator Retroceso(Transform target, Vector3 targetPosition, float duration)
+    {
+        float elapsedTime = 0f;
+        Vector3 initialPosition = target.position;
+
+        while (elapsedTime < duration)
+        {
+            target.position = Vector3.Lerp(initialPosition, targetPosition, elapsedTime / duration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        target.position = targetPosition;
+    }
+
+    
+    void OnDrawGizmos()
+    {
+        // Dibuja el rango de detección
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, detectionRange);
+
+        // Dibuja el rango de ataque
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
     }
 }
